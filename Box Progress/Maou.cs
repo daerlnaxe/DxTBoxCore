60ec35ca84f60106b773ccd3fcd0951c50a95c3f
CODOUL Alexandre <axe.test1@gmail.com>
2021-01-05 07:59:31 +0100

    propre
    propre

--- Box Progress/Maou.cs
+++ Box Progress/Maou.cs
@@ -1,10 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading;
 
 namespace DxTBoxCore.Box_Progress
 {
-    class Maou
+    /// <summary>
+    /// Encapsule a method to make it compatible with progress boxes
+    /// </summary>
+    public class Maou<T> : I_ASBaseC
     {
+        public CancellationTokenSource TokenSource { get; } = new CancellationTokenSource();
+
+        public CancellationToken CancelToken { get; }
+
+        public bool IsPaused { get ; set; }
+
+        public event DoubleDel UpdateTotalProgress;
+        public event StringDel UpdateTotalStatus;
+        public event DoubleDel UpdateProgress;
+        public event StringDel UpdateStatus;
+
+        public Maou()
+        {
+            CancelToken = TokenSource.Token;
+        }
+
+        public Func<I_ASBase, T> ToRun { get; set; }
+
+        public object Run(int timeSleep = 10)
+        {
+            return ToRun(this);
+        }
+
+        public void StopTask()
+        {           
+            TokenSource.Cancel();
+        }
+
+      //  public Func<object> Ending { get; set; }
+
     }
 }
